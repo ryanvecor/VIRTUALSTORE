5b999bb084bcf3f9d2b0eefd408b1758576f0453
ryanvecor <ryanvecor@gmail.com>
2024-08-19 16:34:44 -0500

    Primer commit en la rama develop. Pruebas con método get, paso de parámetros dinámicos por url

--- index.js
+++ index.js
@@ -11,6 +11,56 @@ app.get('/', (req, res)=> { // parámetros del callback: req, request. Y res, re
   res.send('Este es mi server en Express');
 });
 
+/** Nueva ruta: localhost:3000/new-endpoint */
+app.get('/new-endpoint', (req, res)=> { // Ejemplos de endpoint: users, products, categories
+  res.send('Aquí se debe mostrar el resultado de la implementación del new-endpoint ');
+});
+
+/** USO DEL FORMATO JSON
+ * Cambiamos el formato para presentar el resultado en la página.
+ * Por ejemplo, para el Frontend y aplicaciones móviles se usa el formato JSON
+ */
+app.get('/products', (req, res)=> { // Ejemplos de endpoint: users, products, categories
+  res.json([
+    {
+      name: 'Computer X',
+      price: 4000
+    },
+
+    {
+      name: 'Laptop X',
+      price: 5000
+    },
+
+    {
+      name: 'Television X',
+      price: 2000
+    }
+  ]);
+});
+
+
+app.get('/products/:id', (req, res)=> { // Método get, recibiendo el parámetro id
+  const { id } = req.params; //destructuración: de todos los parámetros que tenga el objeto .params solo requerimos el id
+  res.json(
+    {
+      id,
+      name: 'Computer X',
+      price: 4000
+    }
+  );
+});
+
+//relacionando entidades: productos con categorías
+app.get('/categories/:categoryId/products/:productId', (req, res)=> {
+  const { categoryId, productId } = req.params;
+  res.json(
+    {
+      categoryId,
+      productId
+    }
+  );
+});
 
 //le decimos a la app el puerto por el que debe escuchar
 app.listen(port, () => { //callback sin parámetros
